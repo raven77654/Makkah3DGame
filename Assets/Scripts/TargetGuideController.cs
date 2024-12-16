50e67c0130fb5819ab8b06f818b9d67ef732c2f0
sadiaKamal <sadiakamal2468@gmail.com>
2024-12-17 02:21:52 +0500

    new2

---
+++ Assets/Scripts/TargetGuideController.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class TargetGuideController : MonoBehaviour
+{
+    public GameObject[] targets; // Array of target objects
+    private int currentTargetIndex = 0;
+
+    private GameObject activePanels; // Active language panels
+
+    private void Start()
+    {
+        activePanels = FindObjectOfType<LanguagePanelsController>().GetActivePanels();
+
+        if (targets.Length > 0)
+        {
+            ActivateTarget(0); // Start with the first target
+        }
+    }
+
+    public void OnTargetReached()
+    {
+        if (currentTargetIndex < targets.Length)
+        {
+            // Display the corresponding panel
+            if (activePanels != null)
+            {
+                Transform panel = activePanels.transform.GetChild(currentTargetIndex);
+                if (panel != null) panel.gameObject.SetActive(true);
+            }
+
+            currentTargetIndex++;
+
+            // Activate the next target if it exists
+            if (currentTargetIndex < targets.Length)
+            {
+                ActivateTarget(currentTargetIndex);
+            }
+        }
+    }
+
+    private void ActivateTarget(int index)
+    {
+        for (int i = 0; i < targets.Length; i++)
+        {
+            targets[i].SetActive(i == index); // Activate only the current target
+        }
+        Debug.Log("Activated target: " + targets[index].name);
+    }
+}
